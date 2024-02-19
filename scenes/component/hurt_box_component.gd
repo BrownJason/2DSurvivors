2ea96e060204d8ec0ceb60674f548d8fbd0bd9d2
Jason <jbjbro6@gmail.com>
2024-02-19 10:16:48 -0500

    Additional animations, added shader for flashing and floating text

--- scenes/component/hurt_box_component.gd
+++ scenes/component/hurt_box_component.gd
@@ -1,7 +1,8 @@
 extends Area2D
 class_name HurtBoxComponent
 
-@export var health_component: HealthComponent 
+@export var health_component: HealthComponent
+var float_text_scene: PackedScene = preload("res://scenes/ui/floating_text.tscn")
 
 func _ready():
 	area_entered.connect(on_area_entered)
@@ -15,3 +16,9 @@ func on_area_entered(area: Area2D):
 		
 	var hit_box_component = area as HitBoxComponent
 	health_component.damage(hit_box_component.damage)
+	
+	var floating_text = float_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+	floating_text.global_position = global_position + Vector2.UP * 16
+	floating_text.start(str(hit_box_component.damage))
+	 
