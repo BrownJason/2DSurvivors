2d5419440719db224b8077eed27fbd700b275487
Jason <jbjbro6@gmail.com>
2024-02-20 15:50:02 -0500

    Fix multiple level upgrades

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -1,6 +1,6 @@
 extends Node
 
-signal experience_updated(current_experience: float, target_experience: float)
+signal experience_updated(current_experience: float, target_experience: float, leveled: bool)
 signal level_up(new_level: int)
 
 const EXPERIENCE_GROWTH_FACTOR = 1.2
@@ -14,14 +14,19 @@ func _ready():
 
 func increment_experience(number: float):
 	current_experience += number
-	experience_updated.emit(min(current_experience, target_experience), target_experience)
-	
+
+	if current_experience < target_experience:
+		experience_updated.emit(current_experience, target_experience, false)
+
 	while current_experience >= target_experience:
 		current_level += 1
 		current_experience -= target_experience
 		target_experience *= EXPERIENCE_GROWTH_FACTOR
-		experience_updated.emit(current_experience, target_experience)
-		level_up.emit(current_level)
+		experience_updated.emit(current_experience, target_experience, true)
+
+
+func leveled_up():
+	level_up.emit(current_level)
 
 func on_experience_vial_collected(number: float):
 	increment_experience(number)
--- scenes/ui/experience_bar.gd
+++ scenes/ui/experience_bar.gd
@@ -5,18 +5,20 @@ extends CanvasLayer
 @onready var level_label = $MarginContainer/HBoxContainer/LevelLabel
 
 var percent = 0.0
+var counter = 0
 
 func _ready():
 	progress_bar.value = 0
 	experience_manager.experience_updated.connect(on_experience_updated)
 	level_label.text = str(experience_manager.current_level)
 	
-func on_experience_updated(current_experience: float, target_experience: float):
+func on_experience_updated(current_experience: float, target_experience: float, leveled_ups: bool):
 	percent = current_experience / target_experience
 	var tween = create_tween().set_trans(Tween.TRANS_CIRC)
-	if current_experience > progress_bar.max_value:
-		tween.tween_property(progress_bar, "value", percent, 0.3)
-	else:
-		tween.tween_property(progress_bar, "value", percent, 0.3).from(0.0)
+	tween.tween_property(progress_bar, "value", percent, 0.5)
+	if leveled_ups:
+		tween.tween_callback(experience_manager.leveled_up)
+	tween.tween_interval(0.2)
 	level_label.text = str(experience_manager.current_level)
 
+	
